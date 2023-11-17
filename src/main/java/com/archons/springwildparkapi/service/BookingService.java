86770f3b77fb3da432a61f295646eea833b8b6f7
cxrvxxxx <brentempasisx3@gmail.com>
2023-11-17 09:34:03 +0800

    modified booking controller

--- src/main/java/com/archons/springwildparkapi/service/BookingService.java
+++ src/main/java/com/archons/springwildparkapi/service/BookingService.java
@@ -1,15 +1,15 @@
 package com.archons.springwildparkapi.service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.archons.springwildparkapi.exceptions.BookingNotFoundException;
+import com.archons.springwildparkapi.exceptions.InsufficientPrivillegesException;
 import com.archons.springwildparkapi.model.AccountEntity;
 import com.archons.springwildparkapi.model.BookingEntity;
-import com.archons.springwildparkapi.model.ParkingAreaEntity;
+import com.archons.springwildparkapi.model.Role;
 import com.archons.springwildparkapi.repository.BookingRepository;
 
 @Service
@@ -21,54 +21,61 @@ public class BookingService {
         this.bookingRepository = bookingRepository;
     }
 
-    public List<BookingEntity> getBookingsByAccount(AccountEntity booker) {
-        Iterable<BookingEntity> iterable = bookingRepository.findAll();
-        List<BookingEntity> bookingList = new ArrayList<>();
-
-        for (BookingEntity booking : iterable) {
-            if (booking.getBooker().equals(booker)) {
-                bookingList.add(booking);
-            }
-        }
-
-        return bookingList;
-    }
-
-    public List<BookingEntity> getBookingByArea(ParkingAreaEntity parkingArea) {
-        Iterable<BookingEntity> iterable = bookingRepository.findAll();
-        List<BookingEntity> bookingList = new ArrayList<>();
-
-        for (BookingEntity booking : iterable) {
-            if (booking.getArea().equals(parkingArea)) {
-                bookingList.add(booking);
-            }
-        }
-
-        return bookingList;
-    }
-
-    public Optional<BookingEntity> getBookingById(int bookingId) {
-        return bookingRepository.findById(bookingId);
-    }
-
-    public Optional<BookingEntity> updateBooking(BookingEntity updatedBooking) {
-        Optional<BookingEntity> existingBooking = bookingRepository.findById(updatedBooking.getId());
-
-        if (existingBooking.isPresent()) {
-            return Optional.of(bookingRepository.save(updatedBooking));
-        }
-
-        return Optional.ofNullable(null);
-    }
-
-    public boolean deleteBooking(int bookingId) {
+    public Optional<BookingEntity> getBookingById(AccountEntity requester, int bookingId)
+            throws InsufficientPrivillegesException, BookingNotFoundException {
         Optional<BookingEntity> existingBooking = bookingRepository.findById(bookingId);
 
         if (existingBooking.isPresent()) {
-            bookingRepository.delete(existingBooking.get());
-            return true;
+            BookingEntity booking = existingBooking.get();
+
+            if (booking.getBooker() != requester && requester.getRole() != Role.ADMIN) {
+                throw new InsufficientPrivillegesException();
+            }
+
+            return Optional.of(booking);
+        } else {
+            throw new BookingNotFoundException();
+        }
+    }
+
+    public Optional<BookingEntity> addBooking(AccountEntity requester, BookingEntity newBooking)
+            throws InsufficientPrivillegesException {
+        if (!requester.equals(newBooking.getBooker()) && requester.getRole() != Role.ADMIN) {
+            throw new InsufficientPrivillegesException();
         }
 
-        return false;
+        return Optional.of(bookingRepository.save(newBooking));
+    }
+
+    public Optional<BookingEntity> updateBooking(AccountEntity requester, BookingEntity updatedBooking, int bookingId)
+            throws InsufficientPrivillegesException, BookingNotFoundException {
+        if (!requester.equals(updatedBooking.getBooker()) && requester.getRole() != Role.ADMIN) {
+            throw new InsufficientPrivillegesException();
+        }
+
+        Optional<BookingEntity> existingBooking = getBookingById(requester, bookingId);
+
+        if (!existingBooking.isPresent()) {
+            throw new BookingNotFoundException();
+        }
+
+        return Optional.of(bookingRepository.save(updatedBooking));
+    }
+
+    public void deleteBooking(AccountEntity requester, int bookingId)
+            throws InsufficientPrivillegesException, BookingNotFoundException {
+        Optional<BookingEntity> existingBooking = bookingRepository.findById(bookingId);
+
+        if (existingBooking.isPresent()) {
+            BookingEntity booking = existingBooking.get();
+
+            if (booking.getBooker() != requester && requester.getRole() != Role.ADMIN) {
+                throw new InsufficientPrivillegesException();
+            }
+
+            bookingRepository.delete(booking);
+        } else {
+            throw new BookingNotFoundException();
+        }
     }
 }
