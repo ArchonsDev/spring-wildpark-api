27f90c4d6d45d800c8163f3a06ca5533fef17488
Harold II Jamiro <99066605+IceAgeEmpire@users.noreply.github.com>
2023-11-21 23:56:10 +0800

    Update PaymentControllerV1.java

--- src/main/java/com/archons/springwildparkapi/controller/v1/PaymentControllerV1.java
+++ src/main/java/com/archons/springwildparkapi/controller/v1/PaymentControllerV1.java
@@ -1,11 +1,20 @@
 package com.archons.springwildparkapi.controller.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.archons.springwildparkapi.model.PaymentEntity;
 import com.archons.springwildparkapi.service.PaymentService;
 
+import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/payments")
 public class PaymentControllerV1 {
@@ -24,5 +33,75 @@ public class PaymentControllerV1 {
     public PaymentControllerV1(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
+    @GetMapping("/print")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("OKAY!");
+    }
+    /*
+    @PostMapping("/")
+    public ResponseEntity<Optional<PaymentEntity>> addPayment(@RequestBody AccountEntity requester,
+            @RequestBody PaymentEntity payment) {
+        try {
+            return ResponseEntity.ok(paymentService.addPayment(requester, payment));
+        } catch (InsufficientPrivilegesException ex) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+     */
+    @GetMapping("/{paymentId}")
+    public ResponseEntity<Optional<PaymentEntity>> getPaymentById(@PathVariable int paymentId) {
+            return ResponseEntity.ok(paymentService.getPaymentById(paymentId));
+    }
+    @PutMapping("/{paymentId}")
+    public ResponseEntity<Optional<PaymentEntity>> updatePayment(
+            @RequestBody PaymentEntity updatedPayment, @RequestBody int paymentId) {
+        return ResponseEntity.ok(paymentService.updatePayment(updatedPayment));
+    }
+    @DeleteMapping("/{paymentId}")
+    public ResponseEntity<Void> deletePayment(@PathVariable int paymentId) {
+            if(paymentService.deletePayment(paymentId)){
+                return ResponseEntity.ok().build();
+            }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+    /* 
+    //naa try catch
+    @GetMapping("/{paymentId}")
+    public ResponseEntity<Optional<PaymentEntity>> getPaymentById(
+            @PathVariable int paymentId) {
+                System.out.println("getPaymnetById: " + paymentId);
+        try {
+            return ResponseEntity.ok(paymentService.getPaymentById(paymentId));
+        } catch (InsufficientPrivilegesException ex) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        } 
+    }
+    @PutMapping("/{paymentId}")
+    public ResponseEntity<Optional<PaymentEntity>> updatePayment(
+            @RequestBody PaymentEntity updatedPayment, @PathVariable int paymentId) {
+        try {
+            return ResponseEntity.ok(paymentService.updatePayment(updatedPayment));
+        } catch (InsufficientPrivilegesException ex) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+    @DeleteMapping("/{paymentId}")
+    public ResponseEntity<Void> deletePayment(@RequestParam int paymentId) {
+        try {
+            if(paymentService.deletePayment(paymentId)){
+                return ResponseEntity.ok().build();
+            }
+        } catch (InsufficientPrivilegesException ex) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
 
+     */
+    /*Testing path variable
+    @GetMapping("/{paymentId}")
+	public String getAllPayment(@PathVariable int paymentId){
+        return "The id is: " + paymentId;
+	}
+    */
+    //Testing
 }
